e3ffc790e7c4eaf083597e9406966b36e1b43fa9
João Silva <joao.silva@moxy.studio>
2019-10-11 12:15:30 +0100

    test: add tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,47 +1,68 @@
 'use strict';
 
-// const compileNodeModulesPlugin = require('.');
+const compileNodeModulesPlugin = require('../index');
 
-// const webpackConfig = {
-//     module: {
-//         rules: [
-//             {
-//                 test: /\.(tsx|ts|js|mjs|jsx)$/,
-//                 include: [
-//                     '/path/to/project',
-//                     /next[\\/]dist[\\/]next-server[\\/]lib/,
-//                     /next[\\/]dist[\\/]client/,
-//                     /next[\\/]dist[\\/]pages/,
-//                     /[\\/](strip-ansi|ansi-regex)[\\/]/,
-//                 ],
-//                 exclude: () => {},
-//                 use: {
-//                     loader: 'next-babel-loader',
-//                     options: {
-//                         isServer: true,
-//                         hasModern: false,
-//                         distDir: '/path/to/project/.next',
-//                         cwd: '/path/to/project',
-//                         cache: true,
-//                     },
-//                 },
-//             },
-//         ],
-//     },
-// };
+const createWebpackConfig = () => ({
+    module: {
+        rules: [
+            {
+                test: /\.(tsx|ts|js|mjs|jsx)$/,
+                include: [
+                    '/path/to/project',
+                    /next[\\/]dist[\\/]next-server[\\/]lib/,
+                    /next[\\/]dist[\\/]client/,
+                    /next[\\/]dist[\\/]pages/,
+                    /[\\/](strip-ansi|ansi-regex)[\\/]/,
+                ],
+                exclude: () => {},
+                use: {
+                    loader: 'next-babel-loader',
+                    options: {
+                        isServer: true,
+                        hasModern: false,
+                        distDir: '/path/to/project/.next',
+                        cwd: '/path/to/project',
+                        cache: true,
+                    },
+                },
+            },
+        ],
+    },
+});
 
 it('should duplicate the default JS rule', () => {
+    const config = compileNodeModulesPlugin()().webpack(createWebpackConfig());
 
+    expect(config.module.rules).toMatchSnapshot();
 });
 
 it('should throw if the JS rule was not found', () => {
+    const noGoodRuleConfig = createWebpackConfig();
 
+    noGoodRuleConfig.module.rules[0].use.loader = 'bad-loader';
+
+    expect(() => compileNodeModulesPlugin()().webpack(noGoodRuleConfig)).toThrow(/JS rule/);
 });
 
 it('should allow to override the rule\'s test, include and exclude properties', () => {
+    const options = {
+        test: 'my-test',
+        include: 'my-include',
+        exclude: 'my-exclude',
+    };
 
+    const config = compileNodeModulesPlugin(options)().webpack(createWebpackConfig());
+
+    expect(config.module.rules).toMatchSnapshot();
 });
 
 it('should call nextConfig webpack if defined', () => {
+    const nextConfig = {
+        webpack: jest.fn(() => 'foo'),
+    };
 
+    const config = compileNodeModulesPlugin()(nextConfig).webpack(createWebpackConfig());
+
+    expect(nextConfig.webpack).toHaveBeenCalledTimes(1);
+    expect(config).toBe('foo');
 });
